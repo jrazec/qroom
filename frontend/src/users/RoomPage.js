50ef73a6679468ebc12afcacac16ccc768a0b604
zvincent07 <vincentlaylo07@gmail.com>
2024-10-11 11:57:35 +0800

    created room and feedback page

---
+++ frontend/src/users/RoomPage.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import './RoomPage.css';
+
+function RoomPage() {
+
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+
+        <div className="social-icons mt-5">
+          <a href="#" className="social-icon">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fa fa-envelope"></i>
+          </a>
+          <a href="#" className="social-icon">
+          <i className="fa fa-twitter"></i>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default RoomPage;
