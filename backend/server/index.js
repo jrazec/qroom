183d16a88712fce87fe0e7fe2e1eb6d60e2547ed
jrazec <johnrazeagno@gmail.com>
2024-10-12 00:45:34 +0800

    add api routes in server, to be transferred in controllers

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,16 +1,51 @@
+// @ts-nocheck
 const express = require("express");
 const dotenv = require('dotenv');
-
+const con = require("../config/db");
 const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser'); // Parsing the req 
 
-const PORT = process.env.PORT || process.env.PROXY || 3001;
 dotenv.config()
 
+const PORT = process.env.PORT || 3001;
+
+
+app.use(cors());
+app.use(bodyParser.json())
+app.get("/",(req,res)=>{
+  res.send({s:"Hi"})
+})
 app.get("/api", (req, res) => {
-    res.json({ message: "asd" });
+  let queryUsers = `SELECT * FROM Users;`;
+  
+  // Execute the query
+  con.query(queryUsers, (err, result) => {
+      if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'An error occurred while fetching data' });  // Send error response
+      } else {
+          res.status(201).json(result);     // Send the result as JSON response
+      }
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-  console.log(process.env.PORT)
+app.post("/api/accounts",(req,res)=>{
+  console.log(req.body.role)
+  const addUsers = `INSERT INTO Users(user_name,first_name,middle_name,last_name,password,role)
+                    VALUES(?,?,?,?,?,?);`;
+  const userData = [`${req.body.userName}`,`${req.body.firstName}`,`${req.body.middleName}`,`${req.body.lastName}`,`${req.body.password}`,`${req.body.role}`];
+  // Execute the query
+  con.query(addUsers,userData, (err, result) => {
+      if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'An error occurred while fetching data' });  // Send error response
+      } else {
+          res.status(201).json({ message: "User added successfully", data: result });
+      }
+  });
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
