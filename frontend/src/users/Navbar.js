988f1cf2627522b6ef0c3f6dc001b42fdf6ee743
zvincent07 <vincentlaylo07@gmail.com>
2024-11-03 14:10:33 +0800

    back the navbar to its original

--- frontend/src/users/Navbar.js
+++ frontend/src/users/Navbar.js
@@ -1,29 +1,9 @@
-// frontend/users/Navbar.js
 import React from 'react';
 import Nav from './Navbar.module.css';
-import { Link, useNavigate, useLocation, useParams } from "react-router-dom";
-import { FaArrowLeft } from 'react-icons/fa';
+import { Outlet, Link, useParams } from "react-router-dom";
 
 function Navbar() {
   const { id } = useParams();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  // Function to handle navigation back to Home page
-  const handleBack = () => {
-    navigate(`/user/home/${id}`);
-  };
-
-  // Determine when to show the back button based on the route
-  const showBackButton = [
-    `/user/schedule/${id}`,
-    `/user/room-search/${id}`,
-    `/user/room-search/${id}/bldg`,
-    `/user/room-search/${id}/floor`,
-    `/user/schedule/${id}/room`,
-    `/user/feedback/${id}`,
-    `/user/settings/${id}`,
-  ].includes(location.pathname);
 
   return (
     <div id="navbar">
@@ -35,16 +15,9 @@ function Navbar() {
           <Link to={`/user/schedule/${id}`} className={Nav['nav-item']}>Schedule</Link>
           <Link to={`/user/room-search/${id}`} className={Nav['nav-item']}>Rooms</Link>
           <Link to={`/user/feedback/${id}`} className={Nav['nav-item']}>Feedback</Link>
-          <Link to={`/user/settings/${id}`} className={Nav['nav-item']}>Settings</Link>
+          <Link to={`/user/settings/${id}`} className={Nav['nav-item']}>Settings</Link> {/* New Settings Link */}
         </nav>
       </header>
-
-      {/* Back to Home Icon Button */}
-      {showBackButton && (
-        <div className={Nav['back-icon-container']}>
-          <FaArrowLeft onClick={handleBack} className={Nav['back-icon']} />
-        </div>
-      )}
 
       {/* Bottom Navbar */}
       <nav className={Nav['bottom-nav']}>
@@ -52,7 +25,7 @@ function Navbar() {
         <a href={`/user/room-search/${id}`}><i className="fa fa-search"></i></a>
         <a href={`/user/schedule/${id}`}><i className="fa fa-calendar"></i></a>
         <a href={`/user/feedback/${id}`}><i className="fa fa-envelope"></i></a>
-        <a href={`/user/settings/${id}`}><i className="fa fa-cog"></i></a>
+        <a href={`/user/settings/${id}`}><i className="fa fa-cog"></i></a> {/* Settings Icon */}
       </nav>
     </div>
   );
