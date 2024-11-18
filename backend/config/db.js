c140ed9f4b092c7fd4de21e8466be3e8d240214c
jrazec <johnrazeagno@gmail.com>
2024-11-18 11:13:50 +0800

    remove host in config/db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,6 @@ dotenv.config()
 // MYSQL CONNECTION
 
 const con = mysql.createConnection({
-    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
