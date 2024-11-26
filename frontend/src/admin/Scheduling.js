a60205f24acdd680ad5f34ad6ecefdc386e00fdf
auclinn <badet.awatin@gmail.com>
2024-11-26 21:26:00 +0800

    Modify scheduling layout, initial

--- frontend/src/admin/Scheduling.js
+++ frontend/src/admin/Scheduling.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./Scheduling.css";
+import styles from "./Scheduling.module.css";
 import { cur } from "../App";
 import Scheduling1 from "./Scheduling1";
 import RoomPDFEditor from "./RoomPDFEditor"
@@ -16,15 +16,24 @@ function Scheduling() {
 
     return (
      <>
-      <div className="dashboard-container">
-      <div className="dashboard-header">
-        <h1 className="dashboard-title">
-          Dashboard <span className="sub-title">Scheduling</span>
-        </h1>
+      <div className={styles["dashboard-container"]}>
+      <div className={styles["dashboard-header"]}>
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                <h1 className="h2 font-weight-bold" styles={{color: '#800000'}}>Scheduling</h1>
+        </div>
+      </div>
+      <div className={styles["circle-buttons"]}>
+      <Link to="/admin/scheduling/sections" class="text-white text-decoration-none"><button className={styles["circle-btn"]}>Manage Sections</button></Link>
+      <Link to="/admin/scheduling/courses" class="text-white text-decoration-none"><button className={styles["circle-btn"]}>Manage Course</button></Link>
+      <Link to="/admin/scheduling/profselect" class="text-white text-decoration-none"><button className={styles["circle-btn"]}>Schedule Class</button></Link>
       </div>
 
-      <div className="department-room-assignment">
+      <div className={styles.deptHeader}>
         <h2>Department Room Assignment</h2>
+        <Link to="/admin/scheduling/4" class="text-white text-decoration-none" id="addBtn"><button className={styles["add-button"]}>+add</button></Link>
+      </div>
+      <div className={styles["department-room-assignment"]}>
+        
         <div>
             <label htmlFor="dept" className="font-weight-bold mr-2">{(dept === '') ? "Choose Department" : dept}</label>
             <select id="dept" className="form-control d-inline-block w-auto mr-3" name="deptSelection" 
@@ -37,20 +46,15 @@ function Scheduling() {
               <option value="cabe">CABE</option>
             </select>
         </div>
-        <div className="room-assignment-grid">
-          <div className="room-column">CICS</div>
-          <div className="room-column">CTE</div>
-          <div className="room-column">CAS</div>
-          <div className="room-column">CABE</div>
+        <div className={styles["room-assignment-grid"]}>
+          <div className={styles["room-column"]}>CICS</div>
+          <div className={styles["room-column"]}>CTE</div>
+          <div className={styles["room-column"]}>CAS</div>
+          <div className={styles["room-column"]}>CABE</div>
         </div>
-        <Link to="/admin/scheduling/4" class="text-white" id="addBtn"><button className="add-button">+add</button></Link>
       </div>
 
-      <div className="circle-buttons">
-      <Link to="/admin/scheduling/sections" class="text-white"><button className="circle-btn">Manage Sections</button></Link>
-      <Link to="/admin/scheduling/courses" class="text-white"><button className="circle-btn">Manage Course</button></Link>
-      <Link to="/admin/scheduling/profselect" class="text-white"><button className="circle-btn">Schedule Class</button></Link>
-      </div>
+      
     </div>
     <RoomPDFEditor />
     </>
