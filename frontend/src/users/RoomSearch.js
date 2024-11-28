1b260af175d6b7fa0e9df7cab0f3cfd1f31b6411
zvincent07 <vincentlaylo07@gmail.com>
2024-11-28 11:47:01 +0800

    fix layout

--- frontend/src/users/RoomSearch.js
+++ frontend/src/users/RoomSearch.js
@@ -16,23 +16,28 @@ function RoomSearch() {
   const [reservationStatus, setReservationStatus] = useState(''); // State for reservation status
   const [currentSchedule, setCurrentSchedule] = useState([]); // State for current schedule
 
-  const dayMap = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // Map days to their index positions
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const { roomid, id, floorid } = useParams();
+  const dayMap = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // Map days to their index positions
+  const days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const { roomid, id } = useParams();
   const navigate = useNavigate();
 
-  // Mapping for building IDs to their respective image files
-  const buildingImages = {
-    1: "/assets/lsb.png", // LEONOR SOLIS BUILDING
-    2: "/assets/vmb.png", // VALERIO MALABANAN BUILDING
-    3: "/assets/abb.png", // ANDRES BONIFACIO BUILDING
-    4: "/assets/gzb.png", // GREGORIO ZARA BUILDING
-  };
-
-  const [buildingImage, setBuildingImage] = useState(""); // State to hold the image URL
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Check if the user is authenticated
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Check if the screen is <= 768px
+    };
+
+    handleResize(); // Call once to set initial state
+    window.addEventListener('resize', handleResize); // Update state on resize
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Check if the user is authenticated by looking for a token in localStorage
     const token = localStorage.getItem('token');
     const loggedInUser = localStorage.getItem('user_name'); // Get logged-in user's username from localStorage
 
@@ -48,31 +53,24 @@ function RoomSearch() {
       navigate('/not-found'); // Redirect to the homepage or NotFound page
       return;
     }
-
-    // Dynamically set the building image based on the selected building (floorid)
-    if (buildingImages[floorid]) {
-      setBuildingImage(buildingImages[floorid]);
-    }
-
-    // Fetch room and user details for the specific room
     if (roomid) { // Ensure roomid is defined before fetching
       fetchData(roomid, getRoom, setSchedule, setUserDetails, getUserSchedule, setUserRole, id);
-      if (schedule.length >= 1) {
+      if(schedule.length >= 1) {
         getRoomSpecific(roomid, setCurrentSchedule);
       }
-    }
-  }, [roomid, id, floorid, navigate]);
 
-  // Handle room occupancy toggle
+    }
+  }, [roomid, id, navigate]);
+
   const handleToggleOccupancy = (e) => {
     if (userRole.toLowerCase() === 'instructor') {
       const currentTime = new Date();
       const currentDay = days[currentTime.getDay()];
       const currentHour = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
-
+  
       // Determine the user's schedule status
       let userSchedule;
-
+  
       if (
         currentSchedule.find(
           (item) =>
@@ -82,6 +80,7 @@ function RoomSearch() {
             item.time_end >= currentHour
         )
       ) {
+        // User can occupy and unoccupy their room
         userSchedule = 'scheduled';
       } else if (
         currentSchedule.find(
@@ -91,13 +90,84 @@ function RoomSearch() {
             item.time_end >= currentHour
         )
       ) {
+        // User can reserve and unreserve their room with a waiting status
         userSchedule = 'waiting';
       } else {
+        // User can occupy and unoccupy the room
         userSchedule = 'none';
       }
-
+      console.log(userSchedule)
+  
       // Handle room status based on user schedule
       if (userSchedule === 'scheduled') {
+        if (roomOccupied) {
+          setRoomOccupied(false);
+          setRoomStatus('Scheduled but not Occupied');
+  
+          const isWithinTimeRange = (startTime, endTime, currentTime) => {
+            const [startHour, startMin] = startTime.split(':').map(Number);
+            const [endHour, endMin] = endTime.split(':').map(Number);
+            const [currentHour, currentMin, currentSec] = currentTime.split(':').map(Number);
+  
+            const start = startHour + startMin / 60;
+            const end = endHour + endMin / 60;
+            const current = currentHour + currentMin / 60 + currentSec / 3600;
+  
+            return current >= start && current <= end;
+          };
+  
+          if (!isWithinTimeRange(userSchedule.time_start, userSchedule.time_end, currentHour)) {
+            setReservationStatus('Outside scheduled time');
+            return;
+          }
+          // Update database to mark room as unoccupied
+        } else {
+          setRoomOccupied(true);
+          setRoomStatus('Occupied');
+          // Update database to mark room as occupied
+        }
+      } else if (userSchedule === 'waiting') {
+        if (roomOccupied) {
+          setRoomOccupied(false);
+          setRoomStatus('Vacant');
+          setReservationStatus('Waiting');
+          // Update database to mark room as not reserved
+        } else {
+          setRoomOccupied(true);
+          setRoomStatus('Reserved');
+          setReservationStatus('Waiting');
+          // Update database to mark room as reserved
+        }
+      } else {
+        // userSchedule === 'none'
+        if (roomOccupied) {
+          setRoomOccupied(false);
+          setRoomStatus('Vacant');
+          // Update database to mark room as unoccupied
+        } else {
+          setRoomOccupied(true);
+          setRoomStatus('Occupied');
+          // Update database to mark room as occupied
+        }
+      }
+    }
+  };
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const currentDay = days[currentTime.getDay()];
+      const currentHour = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+  
+      // Declare userSchedule before logging it
+      const userSchedule = currentSchedule.find(
+        (item) => item.day === currentDay && item.time_start <= currentHour && item.time_end >= currentHour
+      );
+  
+      console.log(userSchedule, "      console.log(userSchedule)");
+  
+      if (userSchedule) {
         if (roomOccupied) {
           setRoomOccupied(false);
           setRoomStatus('Scheduled but not Occupied');
@@ -105,47 +175,37 @@ function RoomSearch() {
           setRoomOccupied(true);
           setRoomStatus('Occupied');
         }
-      } else if (userSchedule === 'waiting') {
-        if (roomOccupied) {
-          setRoomOccupied(false);
-          setRoomStatus('Vacant');
-          setReservationStatus('Waiting');
-        } else {
-          setRoomOccupied(true);
-          setRoomStatus('Reserved');
-          setReservationStatus('Waiting');
-        }
       } else {
-        if (roomOccupied) {
-          setRoomOccupied(false);
-          setRoomStatus('Vacant');
-        } else {
-          setRoomOccupied(true);
-          setRoomStatus('Occupied');
-        }
+        setReservationStatus('Waiting');
+        setRoomStatus('Vacant');
       }
-    }
-  };
+    }, 60000); // Check every minute
+  
+    return () => clearInterval(interval);
+  }, [schedule, roomOccupied]);
+  
 
   return (
     <div className={roomSearch.app}>
       <Navbar id={id} />
 
       <main className={roomSearch.mainContent}>
+
         <div className={roomSearch.backButton}>
           <button className="btn btn-link">
             <i className="fa fa-arrow-left"></i> Back
           </button>
         </div>
 
-        <div className={`row justify-content-center align-items-center ${roomSearch.mainLayout} mt-1`}>
+        <div 
+        className={`row justify-content-center align-items-center ${roomSearch.mainLayout} mt-1`}
+        >
           {/* Left Section - Room Image and Status */}
-          <div className={`col-md-5 text-center ${roomSearch.leftSection}`}>
+          <div className={`col-md-5 text-center ${roomSearch.leftSection} ${isMobile ? 'mr-4 pr-2' : ''}`}>
             <div className={roomSearch.roomStatus}>
-              {/* Replace with dynamic building image */}
               <img
-                src={buildingImage} // Dynamically load the building image based on the selected floorid
-                alt="Building"
+                src="https://picsum.photos/500/500"
+                alt="Room Status"
                 className={`${roomSearch.roomImage} img-fluid`}
               />
               <h2 className="mt-3">{userDetails[0]?.room_name}</h2>
@@ -156,7 +216,7 @@ function RoomSearch() {
                 {roomStatus === 'Vacant' && <span className="text-success">●</span>}
               </p>
               <button
-                className={`btn mt-2 ${userRole.toLowerCase() === 'student' ? 'btn-secondary' : 'btn-primary'}`}
+                className={`btn mt-2 ${userRole.toLowerCase() === 'student' ? 'btn-secondary' : 'btn-primary'} ${isMobile ? 'mr-2' : ''} mb-2`} 
                 onClick={handleToggleOccupancy}
                 disabled={userRole.toLowerCase() === 'student'} // Disable button for students
               >
@@ -168,32 +228,31 @@ function RoomSearch() {
 
           {/* Right Section - Schedule and User Details */}
           <div className={`col-md-6 ${roomSearch.rightSection}`}>
-            {/* Schedule Wrapper */}
             <div className={roomSearch.scheduleWrapper}>
-              {/* User Details */}
-              <div className={roomSearch.leftSched}>
-                {userDetails === undefined ? (
-                  <p>No Schedule Yet</p>
-                ) : (
-                  userDetails.map((user, index) => (
-                    <div key={index} className={`${roomSearch.scheduleUser} mb-3`}>
-                      <img
-                        src={user.image}
-                        alt="User"
-                        style={{ objectFit: 'cover' }}
-                        className={`${roomSearch.userImage} img-fluid`}
-                      />
-                      <div className={roomSearch.userDetails}>
-                        <p><strong>Instructor:</strong> {user.first_name} {user.middle_name} {user.last_name}</p>
-                        <p><strong>Section:</strong> {user.section_name}</p>
-                        <p><strong>Time:</strong> {user.time_start} - {user.time_end}</p>
-                      </div>
+            <div className={roomSearch.leftSched}>
+              {/* Multiple User Details */}
+              {userDetails === undefined ? (
+                <p>No Schedule Yet</p>
+              ) : (
+              
+                userDetails.map((user, index) => (
+                  <div key={index} className={`${roomSearch.scheduleUser} mb-3`}>
+                    <img
+                      src={user.image}
+                      alt="User"
+                      style={{ objectFit: 'cover' }}
+                      className={`${roomSearch.userImage} img-fluid`}
+                    />
+                    <div className={roomSearch.userDetails}>
+                      <p><strong>Instructor:</strong> {user.first_name} {user.middle_name} {user.last_name}</p>
+                      <p><strong>Section:</strong> {user.section_name}</p>
+                      <p><strong>Time:</strong> {user.time_start} - {user.time_end}</p>
                     </div>
-                  ))
-                )}
-              </div>
-              {/* Schedule Content */}
-              <div className={roomSearch.scheduleContainer}>
+                  </div>
+                ))
+              )}
+            </div>
+              <div className={`${roomSearch.scheduleContainer} mt-5`}>
                 <div className={`${roomSearch.days} d-flex justify-content-between`}>
                   {dayMap.map((day, index) => (
                     <div key={index} className={roomSearch.dayLabel}>
@@ -229,7 +288,9 @@ function RoomSearch() {
                 </div>
               </div>
             </div>
-            {/* Optional Previous Button */}
+            
+
+            {/* Previous Button */}
             {/* <div className={roomSearch.previousButton}>
               <button className="btn btn-outline-dark">Previous</button>
             </div> */}
